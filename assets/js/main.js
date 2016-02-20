b7289950b2de3992628c0682a5434e9da8082598
Thomas-Ko <tomkolodzi@gmail.com>
2016-02-20 14:39:06 -0500

    Create basic closure function inside for-loop
    Allows every checkbox to behave as expected

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,52 @@
-$("input:checkbox").click(function(){
-	var value =$(this).val();
+var ingredients = [
+	{
+		name: "BBQ Sauce (top)",
+		imgId: "bbqTopSauceImg",
+		checkBoxId: "bbqTopSauceCheck",
+	},
+	{
+		name: "Hot Sauce",
+		imgId: "hotSauceImg",
+		checkBoxId: "hotSauceCheck",
+	},
+	{
+		name: "Ranch Dressing",
+		imgId: "ranchDressingImg",
+		checkBoxId: "ranchDressingCheck",
+	},
 	
-});
+];
 
-$("#bbqTopSauceCheck").click(function(){
-	$("#bbqTopSauceImg").toggleClass("display-off");
-});
+for (var i = 0; i<ingredients.length;i++){
+	(function(j){
+		var imgId = ingredients[j].imgId;
+		var imgDiv = document.getElementById(imgId);
 
-$("#hotSauceCheck").click(function(){
-	$("#hotSauceImg").toggleClass("display-off");
-});
+		var checkBoxId = ingredients[j].checkBoxId;
+		var checkBoxDiv = document.getElementById(checkBoxId);
+		
+		checkBoxDiv.addEventListener("click",function() {
+			
+			if (checkBoxDiv.checked){
+				imgDiv.classList.remove("display-off");
+			} else {
+				imgDiv.classList.add("display-off");
+			}
 
-$("#ranchDressingCheck").click(function(){
-	$("#ranchDressingImg").toggleClass("display-off");
-});
+		});
+	}(i));
+	//Above is a closure. Without one, the checkboxes would only be refering to the last i in the for-loop.
+}
 
+//Old Workable code
+// $("#bbqTopSauceCheck").click(function(){
+// 	$("#bbqTopSauceImg").toggleClass("display-off");
+// });
+
+// $("#hotSauceCheck").click(function(){
+// 	$("#hotSauceImg").toggleClass("display-off");
+// });
+
+// $("#ranchDressingCheck").click(function(){
+// 	$("#ranchDressingImg").toggleClass("display-off");
+// });
