341554a6c2249ce5ad773cd2feebcfa8dc014ef5
Thomas-Ko <tomkolodzi@gmail.com>
2016-02-20 13:07:46 -0500

    Rename html ids / Add very very basic workable checkbox functionability
    The code in the js file needs to be DRY, but the basics are working

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$("input:checkbox").click(function(){
+	var value =$(this).val();
+	
+});
+
+$("#bbqTopSauceCheck").click(function(){
+	$("#bbqTopSauceImg").toggleClass("display-off");
+});
+
+$("#hotSauceCheck").click(function(){
+	$("#hotSauceImg").toggleClass("display-off");
+});
+
+$("#ranchDressingCheck").click(function(){
+	$("#ranchDressingImg").toggleClass("display-off");
+});
+
